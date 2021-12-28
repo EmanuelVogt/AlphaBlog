37a558bc5cfd703447705c8e1767edc805f9c1e4
EmanuelVogt <contatoemanuel3@gmail.com>
2021-12-28 01:04:28 -0300

    add edit and update article

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root 'articles#index'
   get 'articles/show'
-  resources :articles
+  resources :articles, only: [:show, :index, :new, :create, :edit, :update]
 end
