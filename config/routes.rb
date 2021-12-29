b1665e986b52802a2ffa2c4671e67a2b8625e895
EmanuelVogt <contatoemanuel3@gmail.com>
2021-12-29 19:20:30 -0300

    fiz git revert

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get 'articles/show'
-  resources :articles, only: [:show, :index, :new, :create, :edit, :update, :destroy]
+  resources :articles
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
 end
