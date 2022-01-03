35f5d48eff98e338defc3ce7c6ad79ea2e4bb189
EmanuelVogt <contatoemanuel3@gmail.com>
2022-01-02 23:47:42 -0300

    create user index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -33,6 +33,10 @@ class UsersController < ApplicationController
     end
   end
 
+  def index
+    @users = User.all
+  end
+
   private
   def user_params
     params.require(:user).permit(:username, :email, :password)
